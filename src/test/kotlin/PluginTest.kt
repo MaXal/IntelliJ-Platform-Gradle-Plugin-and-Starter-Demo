8e0b41151c6d44a5524d2ccbd9c20b256799a4d9
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-09-24 16:12:58 +0200

    Reuse new context creation

--- src/test/kotlin/PluginTest.kt
+++ src/test/kotlin/PluginTest.kt
@@ -2,8 +2,9 @@ import com.intellij.driver.sdk.ui.components.welcomeScreen
 import com.intellij.driver.sdk.ui.shouldBe
 import com.intellij.ide.starter.driver.engine.runIdeWithDriver
 import com.intellij.ide.starter.ide.IdeProductProvider
-import com.intellij.ide.starter.junit5.newContext
+import com.intellij.ide.starter.models.TestCase
 import com.intellij.ide.starter.plugins.PluginConfigurator
+import com.intellij.ide.starter.project.NoProject
 import com.intellij.ide.starter.runner.Starter
 import org.junit.jupiter.api.Test
 import kotlin.io.path.Path
@@ -11,7 +12,7 @@ import kotlin.io.path.Path
 class PluginTest {
     @Test
     fun simpleTest() {
-        Starter.newContext(IdeProductProvider.IU, "testExample").apply {
+        Starter.newContext("testExample", TestCase(IdeProductProvider.IU, NoProject) ).apply {
             val pathToPlugin = System.getProperty("path.to.build.plugin")
             PluginConfigurator(this).installPluginFromPath(Path(pathToPlugin))
         }.runIdeWithDriver().useDriverAndCloseIde {
